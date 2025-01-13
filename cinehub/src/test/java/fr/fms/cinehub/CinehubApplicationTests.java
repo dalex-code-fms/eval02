0c7a6259c365ab98f7ba4d688710144ab7bdb714
Ferreira-RodriguesD <david.ferreira-rodrigues@fms-ea.com>
2025-01-13 15:41:22 +0100

    initial commit with spring initializer

---
+++ cinehub/src/test/java/fr/fms/cinehub/CinehubApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.fms.cinehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
