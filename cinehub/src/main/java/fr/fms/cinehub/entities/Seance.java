94fdb55029c0b1bea0aa85f3a2ba445a2c6a6df6
Ferreira-RodriguesD <david.ferreira-rodrigues@fms-ea.com>
2025-01-14 10:42:06 +0100

    created Entity 'Seance'

---
+++ cinehub/src/main/java/fr/fms/cinehub/entities/Seance.java
@@ -0,0 +1,27 @@
+package fr.fms.cinehub.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor @ToString
+public class Seance {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private LocalDateTime dateTime;
+
+    @ManyToOne
+    private Movie movie;
+    
+    @ManyToOne
+    private Cinema cinema;
+}
