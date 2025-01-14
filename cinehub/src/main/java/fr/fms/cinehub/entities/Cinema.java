95f1b9dc515e465b58e8b33e73acedb4e7211b81
Ferreira-RodriguesD <david.ferreira-rodrigues@fms-ea.com>
2025-01-14 10:18:54 +0100

    created entity 'Cinema'

--- cinehub/src/main/java/fr/fms/cinehub/entities/Cinema.java
+++ cinehub/src/main/java/fr/fms/cinehub/entities/Cinema.java
@@ -8,28 +8,29 @@ import lombok.ToString;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.List;
 
 @Entity
 @Data @NoArgsConstructor @AllArgsConstructor @ToString
-public class City {
+public class Cinema {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotNull
-    @Size(min = 1, max = 50)
+    @Size(min = 3, max = 20)
     private String name;
 
     @NotNull
-    @Size(min = 3, max = 20)
-    private String department;
+    @Size(min = 5, max = 30)
+    private String address;
 
-    @NotNull
-    @Size(min = 4, max = 18)
-    private String country;
+    @ManyToOne
+    private City city;
 
-    @OneToMany(mappedBy = "city")
-    private List<Cinema> cinemas;
-
+    @ManyToMany
+    @JoinTable(
+            name = "cinema_movie",
+            joinColumns = @JoinColumn(name = "cinema_id"),
+            inverseJoinColumns = @JoinColumn(name = "movie_id"))
+    private List<Movie> movies;
 }
